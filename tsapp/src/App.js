abb6ccd5304ba4acc83a8838d361277f78968bba
Rhema7776 <rhemachukwumah7@gmail.com>
2022-11-04 07:18:28 +0100

    second commit

--- tsapp/src/App.js
+++ tsapp/src/App.js
@@ -7,11 +7,15 @@ import noun_Designer_1469595 from './noun_Designer_1469595.png';
 import noun_UiandUx_907062 from './Icons and Images/Tech Studio images/noun_Ui and Ux_907062.png';
 import Group182 from './Icons and Images/Tech Studio images/Group 182.png';
 import noun_datascience_2475093 from './Icons and Images/Tech Studio images/noun_data science_2475093.png';
+import page15 from './page15';
+import BlackBusinessman6 from './Icons and Images/Tech Studio images/Black Businessman-6.png';
+import Group25354 from './Icons and Images/Tech Studio images/Group 25354.png';
 
 function App(){
   return (
     <div className="App">
-      <div className="sec_1 container_fluid">
+      
+      <div className="sec_1 container">
         <div className="find_div">
           <h3 className="firsth2">Find the best talents to grow your team</h3>
           <p className="p_1">
@@ -24,72 +28,76 @@ function App(){
            <input className="input1" type="text" placeholder="Your email" />
            <input className="inputbtn" type="submit" placeholder="Get Started" />
           </form>
-          <img src="{RepeatGrid47}" alt=""/>
+          <img className='img_dots1 d-xl-none' src={RepeatGrid47} alt=""/>
         </div>
         
       </div>
-      <div className="sec_2 container_fluid">
-        
-        <h3 className="HTW">How Techstudio works</h3>
-        <p>for employers</p>
-        <div className="sec_2div">
-          <div className="sec_2a">
-            <img src="{Group25353}" alt=""/>
-            <h5 className="htw">Post a job</h5>
-            <p className="para">
-              Create an account and a befitting profile to attract job offers with ease. Your profile shows employers what makes you the right fit.
-            </p>
+      <div className="sec_2 ">
+        <div className='sec2 container'>
+          <div className='container_fluid'>
+            <h3 className="HTW">How Techstudio works</h3>
+            <p>for employers</p>
           </div>
-          <div className="sec_2a">
-            <img src="{Group 25353.png}" alt=""/>
-            <h5 className="htw">Review & shortlist</h5>
-            <p className="para">
-              Create a position on Hired and specify the skills and experience you’re looking for.
-            </p>
-          </div>
-          <div className="sec_2a">
-            <img src="{Group 25353.png}" alt=""/>
-            <h5 className="htw">Interview & hire</h5>
-            <p className="para">
-            Create an account and a befitting profile to attract job offers with ease. Your profile shows employers what makes you the right fit.
-            </p>
-          </div>
+          <div className="sec_2div">
+            <div className="sec_2a">
+              <img src={Group25353} alt=""/>
+              <h5 className="htw">Post a job</h5>
+              <p className="para">
+                Create an account and a befitting profile to attract job offers with ease. Your profile shows employers what makes you the right fit.
+              </p>
+            </div>
+            <div className="sec_2a">
+              <img src={BlackBusinessman6} alt=""/>
+              <h5 className="htw">Review & shortlist</h5>
+              <p className="para">
+                Create a position on Hired and specify the skills and experience you’re looking for.
+              </p>
+            </div>
+            <div className="sec_2a">
+              <img src={Group25354} alt=""/>
+              <h5 className="htw">Interview & hire</h5>
+              <p className="para">
+              Create an account and a befitting profile to attract job offers with ease.
+              </p>
+            </div>
 
+          </div>
         </div>
+        
       </div>
       <div className="sec_3 container">
-        <h3 className="HTW2">Hire to fill<br></br> the most in-demand roles</h3>
+        <h3 className="HTW2 container_fluid">Hire to fill<br></br> the most in-demand roles</h3>
         <div className="d_grid">
           <div>
-            <img src="{noun_Designer_1469595}" alt="" />
+            <img className='Hire' src={noun_Designer_1469595} alt="" />
             <h6>Software Engineers</h6>
             <p>
               Learn the user experience, client management, technical, and practical skills you need to launch a career as a user experience (UX) or user interface (UI) designer.
             </p>
           </div>
           <div>
-            <img src="{noun_UiandUx_907062}" alt="" />
+            <img className='Hire' src={noun_UiandUx_907062} alt="" />
             <h6>Front-end Developers</h6>
             <p>
               Learn the user experience, client management, technical, and practical skills you need to launch a career as a user experience (UX) or user interface (UI) designer.
             </p>
           </div>
           <div>
-            <img src="{noun_coding_3152169}" alt="" />
+            <img className='Hire' src={noun_coding_3152169} alt="" />
             <h6>Software Engineers</h6>
             <p>
               Learn the user experience, client management, technical, and practical skills you need to launch a career as a user experience (UX) or user interface (UI) designer.
             </p>
           </div>
           <div>
-            <img src="{Group182}" alt="" />
+            <img className='Hire' src={Group182} alt="" />
             <h6>Software Engineers</h6>
             <p>
               Learn the user experience, client management, technical, and practical skills you need to launch a career as a user experience (UX) or user interface (UI) designer.
             </p>
           </div>
           <div>
-            <img src="{noun_datascience_2475093}" alt="" />
+            <img className='Hire' src={noun_datascience_2475093} alt="" />
             <h6>Software Engineers</h6>
             <p>
               Learn the user experience, client management, technical, and practical skills you need to launch a career as a user experience (UX) or user interface (UI) designer.
@@ -98,24 +106,27 @@ function App(){
         </div>
 
       </div>
-      <div className="sec_4 container_fluid">
-        <div className="find_div">
-          <h3 className="firsth2">Let's help you hire</h3>
-          <p className="p_1">
-            Let our team of recruitment experts find the right talents fit for you. Based on your requirements, we will shorlist talents with matching skills and help you schedule interviews with them. We will assist you every step of the way.
-          </p>
+      <div className='sec_4'>
+        <div className="sec_4a container">
+          <div className="find_div">
+            <h3 className="firsth2">Let's help you hire</h3>
+            <p className="p_1">
+              Let our team of recruitment experts find the right talents fit for you. Based on your requirements, we will shorlist talents with matching skills and help you schedule interviews with them. We will assist you every step of the way.
+            </p>
+          </div>
+          
+          <form className="form2">
+            <input className="input1" type="text" placeholder="Your name" />
+            <input className="input1" type="text" placeholder="Company Name" />
+            <input className="input1" type="text" placeholder="Company Email" />
+            <input className="input1" type="text" placeholder="Where do you want to fill" />
+            <input className="inputbtn" type="submit" placeholder="Get Started" />
+          </form>
+          
+          
         </div>
-        
-        <form className="form2">
-          <input className="input1" type="text" placeholder="Your name" />
-          <input className="input1" type="text" placeholder="Company Name" />
-          <input className="input1" type="text" placeholder="Company Email" />
-          <input className="input1" type="text" placeholder="Where do you want to fill" />
-          <input className="inputbtn" type="submit" placeholder="Get Started" />
-        </form>
-        
-        
       </div>
+      
     </div>
   );
 }
---
+++ tsapp/src/page15.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page15 = () => {
+  return (
+    <div>page15</div>
+  )
+}
+
+export default page15
